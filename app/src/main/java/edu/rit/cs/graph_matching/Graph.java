d858e5b3ad4f3bac62d44d9b6a526e401fe7571f
Alexander Bhalla <xander.bhalla@gmail.com>
2026-01-13 11:31:36 -0500

    Update Graph interface

--- app/src/main/java/edu/rit/cs/graph_matching/Graph.java
+++ app/src/main/java/edu/rit/cs/graph_matching/Graph.java
@@ -1,14 +1,26 @@
 package edu.rit.cs.graph_matching;
 
-public interface UndirectedGraph {
+/**
+ * The model of a graph that the Dani-Hayes algorithm works with. As stated in
+ * their paper, the algorithm requires two primary functions:
+ * <ol>
+ * <li>Checking whether a specific edge exists: {@link #hasEdge(int, int)}</li>
+ * <li>Getting a random neighbor of an edge:
+ * {@link #getRandomNeighbor(int)}</li>
+ * </ol>
+ */
+public interface Graph {
   /**
+   * Gets the total number of vertices present in this graph. This method must
+   * run in O(1) time.
+   *
    * @return the number of vertices in this graph
    */
-  int getVertices();
+  int size();
 
   /**
    * Checks whether an edge exists between two vertices. Vertice order does not
-   * matter.
+   * matter. This method should run in O(1) time.
    *
    * @param vertice1
    *   the first vertice
@@ -16,10 +28,11 @@ public interface UndirectedGraph {
    *   the second vertice
    * @return true iff an undirected edge exists between these two vertices
    */
-  boolean doesEdgeExist(int vertice1, int vertice2);
+  boolean hasEdge(int vertice1, int vertice2);
 
   /**
    * Gets a random neighbor of a vertex, or indicates that no neighbors exist.
+   * This method should run in O(1) time.
    *
    * @param vertice
    *   the vertex
