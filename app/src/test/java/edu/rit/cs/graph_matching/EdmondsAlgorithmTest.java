b09ac95754350615a2d5a32dcf3b232546d235f1
Alexander Bhalla <xander.bhalla@gmail.com>
2026-01-16 03:16:00 -0500

    Add tests for Edmond's using star and star-wheel hybrid graphs

---
+++ app/src/test/java/edu/rit/cs/graph_matching/EdmondsAlgorithmTest.java
@@ -0,0 +1,39 @@
+package edu.rit.cs.graph_matching;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Set;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class EdmondsAlgorithmTest {
+  @ParameterizedTest
+  @ValueSource(ints = {2, 5, 10, 100, 1000, 10000})
+  void starGraphs(int vertices) {
+    Graph g = GraphGenerator.generateStarGraph(new SparseGraphImpl(vertices));
+    EdmondsAlgorithm algorithm = new EdmondsAlgorithm(g);
+    Set<Edge> matching = algorithm.computeMaximumMatching();
+    assertEquals(1, matching.size(), "Star graphs always have a maximum matching of 1 edge");
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+    "5, 2",
+    "10, 2",
+    "10, 3",
+    "10, 4",
+    "10, 5",
+    "100, 25",
+    "100, 49",
+    "1000, 400",
+    "5000, 1000",
+  })
+  void starWheelHybridGraphs(int vertices, int matchingSize) {
+    Graph g = GraphGenerator.generateStarGraphWithMatching(new SparseGraphImpl(vertices), matchingSize);
+    EdmondsAlgorithm algorithm = new EdmondsAlgorithm(g);
+    Set<Edge> matching = algorithm.computeMaximumMatching();
+    assertEquals(matchingSize, matching.size(), "Star-wheel hybrid graphs have a fixed maximum matching size");
+  }
+}
