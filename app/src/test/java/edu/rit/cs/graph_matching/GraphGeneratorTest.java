4f0c053d34b42bf5da769359a9420318a1466b9e
yx <hyxwqy1@gmail.com>
2026-02-26 06:30:15 -0500

    Changed (Updated) the SparseGraphImpl( ) calls to the new AdjacencySetGraph( ) to avoid compilation errors

--- app/src/test/java/edu/rit/cs/graph_matching/GraphGeneratorTest.java
+++ app/src/test/java/edu/rit/cs/graph_matching/GraphGeneratorTest.java
@@ -160,7 +160,7 @@ class GraphGeneratorTest {
     @Test
     void testGenerateLoopGraph1() {
         // Edge case: a graph with a single vertex cannot form a cycle.
-        MutableGraph graph = new SparseGraphImpl(1);
+        MutableGraph graph = new AdjacencySetGraph(1);
         GraphGenerator.generateLoopGraph(graph);
 
         int edgeCount = 0;
@@ -177,7 +177,7 @@ class GraphGeneratorTest {
     void testGenerateLoopGraph2() {
         // Special small case: two vertices should form a single undirected
         // edge.
-        MutableGraph graph = new SparseGraphImpl(2);
+        MutableGraph graph = new AdjacencySetGraph(2);
         GraphGenerator.generateLoopGraph(graph);
 
         int edgeCount = 0;
@@ -197,7 +197,7 @@ class GraphGeneratorTest {
         // General case: a cycle graph on n >= 3 vertices
         // should contain exactly n edges.
         int n = 7;
-        MutableGraph graph = new SparseGraphImpl(n);
+        MutableGraph graph = new AdjacencySetGraph(n);
         GraphGenerator.generateLoopGraph(graph);
 
         int edgeCount = 0;
